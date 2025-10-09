f41d0e39fe11b833a7b1547c66b145f7cac398c9
Dario <dario.mennillo@immaginazioneelavoro.it>
2025-10-09 14:22:51 +0200

    Update Triangoli.java

--- Prj2_MenuGiochi/src/giochi/Triangoli.java
+++ Prj2_MenuGiochi/src/giochi/Triangoli.java
@@ -2,4 +2,34 @@ package giochi;
 
 public class Triangoli {
 
+	//Crea un programma per stampare i seguenti starpattern
+//	Es1:
+//		* * * * *
+//		* * * *
+//		* * *
+//		* *
+//		*
+//
+//		Es2:
+//		*
+//		* *
+//		* * *
+//		* * * *
+//		* * *
+//		* *
+//		*
+//
+//		Es3:
+//		     *
+//		    * *
+//		   * * *
+//		  * * * *
+//		 * * * * *
+//
+//		Es4
+//		     *
+//		    * *
+//		   *   *
+//		  *     *
+//		 * * * * *  
 }
