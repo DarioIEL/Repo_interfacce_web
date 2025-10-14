afdc122f6bbc7ca79a30b9a74c05ba12b0a72d02
Damen89 <dario.mennillo@immaginazioneelavoro.it>
2025-10-14 10:43:20 +0200

    Update Ordinamento.java

--- Prj2_MenuGiochi/src/giochi/Ordinamento.java
+++ Prj2_MenuGiochi/src/giochi/Ordinamento.java
@@ -1,5 +1,14 @@
 package giochi;
 
 public class Ordinamento {
+	//Dato il seguente Array inverti l'ordine degli elementi
+	//[9,3,5,7,1,] -> [1,7,5,3,9]
 
+	//Dato il seguente array ordinali in modo crescente e decrescente
+	// [5, 9, 11, 0, 1, 40]
+	
+	
+	//[5, 9, 6, 10, 7, 11] carino questo da fare dopo 
+	
+	
 }
