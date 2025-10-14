69b9df87f3857dcff6ae588d08c0330d5eebdff9
Damen89 <dario.mennillo@immaginazioneelavoro.it>
2025-10-14 14:18:52 +0200

    update lezione 14/10

---
+++ LezioniTeoriche/bin/esercizi_lezione2/Esercizi2.md
@@ -0,0 +1,18 @@
+### Esercizio 7: Rimuovi duplicati
+Dato l'array `[3, 5, 3, 8, 5, 1]`, crea un nuovo array senza duplicati.
+**Risultato atteso:** `[3, 5, 8, 1]`
+
+### Esercizio 9: Sposta zeri alla fine
+Dato l'array `[0, 3, 0, 5, 8, 0, 2]`, sposta tutti gli zeri alla fine mantenendo l'ordine degli altri elementi.
+**Risultato atteso:** `[3, 5, 8, 2, 0, 0, 0]`
+
+### Esercizio 10: Array palindromo
+Dato l'array `[1, 2, 3, 2, 1]`, verifica se è palindromo (uguale se letto al contrario).
+**Risultato atteso:** true
+
+### Esercizio 11: Selection Sort
+Dato l'array `[64, 25, 12, 22, 11]`, ordinalo usando l'algoritmo Selection Sort (trova il minimo e mettilo in prima posizione, ripeti).
+
+### Esercizio 12: Ruota array a destra
+Dato l'array `[1, 2, 3, 4, 5]`, ruotalo di 2 posizioni a destra.
+**Risultato atteso:** `[4, 5, 1, 2, 3]`
--- Prj2_MenuGiochi/src/giochi/Ordinamento.java
+++ Prj2_MenuGiochi/src/giochi/Ordinamento.java
@@ -1,14 +1,137 @@
 package giochi;
 
+import java.util.Arrays;
+
 public class Ordinamento {
+	//Per ogni esercizio crea un metodo a se stante che richiamerai nel main
+	
 	//Dato il seguente Array inverti l'ordine degli elementi
 	//[9,3,5,7,1,] -> [1,7,5,3,9]
 
-	//Dato il seguente array ordinali in modo crescente e decrescente
+	
+	//Dato il seguente array ordinalo in modo crescente e decrescente
 	// [5, 9, 11, 0, 1, 40]
 	
 	
 	//[5, 9, 6, 10, 7, 11] carino questo da fare dopo 
 	
+	public static int[] esercizio1(int[] arr) {
+		int[] mioArr = arr;
+		int[] invArray = new int[mioArr.length];
+		
+		int j = 0; //contatore
+		
+		System.out.println(Arrays.toString(mioArr) + " Array originale");
+		
+		for(int i = mioArr.length - 1; i  >= 0 ; i--) {
+			int temp = mioArr[i];
+			invArray[j] = temp;
+			j++;
+		}
+		
+//		System.out.println(Arrays.toString(invArray) + "Array inverso");
+		return invArray;
+	}
 	
+	
+	public static void esercizio1() {
+		//RISOLVI QUESTO STESSO ESERCIZIO CON UN WHILE
+		
+		int[] mioArr = {9,3,5,7,1};
+		int[] invArray = new int[mioArr.length];
+		
+		int j = 0; //contatore
+		
+		System.out.println(Arrays.toString(mioArr) + " Array originale");
+		
+		for(int i = mioArr.length - 1; i  >= 0 ; i--) {
+			int temp = mioArr[i];
+			invArray[j] = temp;
+			j++;
+		}
+		
+		System.out.println(Arrays.toString(invArray) + "Array inverso");
+		
+	}
+	
+
+	
+	public static void esercizio2CRESC(int[] arr) {
+		
+		boolean ordine = true;
+		
+		while(ordine) {
+			for(int i = 1; i < arr.length; i++ ) {
+				if(arr[i] < arr[i-1]) {
+					int temp = arr[i];
+					arr[i] = arr[i-1];
+					arr[i-1] = temp;
+					ordine = false;
+				}
+			}
+			
+			for(int i = 1; i < arr.length; i++ ) {
+				if(arr[i] < arr[i-1]) {
+					ordine = true;
+				}
+			}
+		}
+		
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	public static void esercizio2VerDario(int[] arr) {
+		int n = arr.length;
+		for(int i = 0; i < n - 1 ; i++) {
+			for(int j = 0; j < n - 1 - i; j++) {
+				if(arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	public static void esercizio2While(int[] arr) {
+		int i = 0;
+		
+		while(i < arr.length - 1) {
+			int j = 0;
+			while(j < arr.length - 1 - i) {
+				if(arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+				j++;
+			}
+			i++;
+		}
+		
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	
+	
+	public static void esercizio2DESC() {}
+	
+	public static void main(String[] args) {
+		int[] mioArr = {9,3,5,7,1};
+		System.out.println( Arrays.toString(esercizio1(mioArr)) );
+		
+		int[] mioArr2 = {5, 9, 11, 0, 1};
+		esercizio2CRESC(mioArr2);
+		
+	
+		esercizio2VerDario(mioArr2);
+		
+		int[] mioArr3 = {5, 9, 11, 0, 1, 40};
+		esercizio2While(mioArr3);
+		
+		int[] mioArr4 = {5, 9, 6, 10, 7, 11};
+		esercizio2VerDario(mioArr4);
+		
+	}
 }
