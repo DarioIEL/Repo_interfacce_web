b1c32511f9139d665964c3c234a484bdf31f4f54
Dario <dario.mennillo@immaginazioneelavoro.it>
2025-10-28 11:14:10 +0100

    up Banca

--- Prj5_ContoCorrenteMVC/src/model/Banca.java
+++ Prj5_ContoCorrenteMVC/src/model/Banca.java
@@ -1,5 +1,28 @@
 package model;
 
-public class Banca {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class Banca {
+	private String nomeBanca;
+	private List<ContoCorrente> conti;
+	private Map<String, Correntista> correntisti;
+	
+	public Banca(String nomeBanca) {
+		this.nomeBanca = nomeBanca;
+		this.conti = new ArrayList<>();
+		this.correntisti = new HashMap<>();
+	}
+	
+	//getNomeBanca
+	//getOrCreateCorrentista
+	//createConto
+	//getConti
+	//findConto (numeroConto)
+	//eliminaConto (numeroConto) --> questo sfrutta findConto()
+	//getCorrentisti
+	
+	
 }
--- Prj5_ContoCorrenteMVC/src/view/Main.java
+++ Prj5_ContoCorrenteMVC/src/view/Main.java
@@ -2,9 +2,7 @@ package view;
 
 public class Main {
 	public static void main(String[] args) {
-		
-		BancaView bancaView = new BancaView();
-		
+			
 		
 	}
 
